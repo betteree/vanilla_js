f5e566cf8a2698f948e2491febf8aa9419c19534
나은 <kkne1017@gmail.com>
2024-07-03 09:50:16 +0900

    dd

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hi");
+const loginInput= document.querySelector("#login-form input");
+const loginForm= document.querySelector("#login-form");
+
+function onLoginBtnClick()
+{
+    const username = loginInput.value;
+    console.log(username);
+}
+
+loginForm.addEventListener("submit",onLoginSubmit());
+
+onLoginSubmit({})
